0a06cc1bcc73b6d7409111ddbc5935fb11cab936
avalapa <avalapa@uco.edu>
2024-09-20 22:48:38 -0500

    100%

---
+++ project05/project04-05.js
@@ -0,0 +1,40 @@
+/*    JavaScript 7th Edition
+      Chapter 4
+      Project 04-05
+
+      Degrees <-> Radians Coverter
+       Author: adithya and Pranay
+      Date:   09/20/2024 
+
+      Filename: project04-05.js
+ */
+
+
+// Function to convert degrees to radians
+function degreesToRadians(degrees) {
+   return degrees*Math.PI/180;
+   }
+   
+   // Function to convert radians to degrees
+   function radiansToDegrees(radians) {
+   return radians*180/Math.PI;
+   }
+   
+   // Event handler that converts radians to degrees when the input box is changed
+   document.getElementById("rValue").onchange = function() {
+   let radians = document.getElementById("rValue").value;
+   console.log("Radians:", radians, "radians");
+   document.getElementById("aValue").value = formatValue3(radiansToDegrees(radians));
+   }
+   
+   // Event handler that converts degrees to radians when the input box is changed
+   document.getElementById("aValue").onchange = function() {
+   let degrees = document.getElementById("aValue").value;
+   console.log("Degrees:", degrees, "degrees");
+   document.getElementById("rValue").value = formatValue3(degreesToRadians(degrees));
+   }
+   
+   // Function to display a numeric value in the format ##.###
+   function formatValue3(value) {
+   return value.toFixed(3);
+   }
